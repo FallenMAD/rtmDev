6fc945280a163f5f1ee610cbeb23a204c0a08c71
FallenMAD <naruto.yzymaki4@gmail.com>
2025-02-25 14:57:03 +0100

    add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require("node:path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        compress: true,
+        port: 9000,
+        open: true,
     },
     plugins: [
         new MiniCssExtractPlugin({
